a8db1c0df821ceaf08b2742a742facc88972aeaa
eCodeVoyager <mu23172@gmail.com>
2025-11-25 17:10:41 +0600

    feat: Add registration page and update routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import {
   Navigate,
 } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
